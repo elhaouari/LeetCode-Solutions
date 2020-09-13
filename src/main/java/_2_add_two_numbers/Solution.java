4162791cbb87ce39e2023d771c7ce9f66a5868b7
Nabil <n.elhaououari@gmail.com>
2020-09-13 23:13:16 +0100

    Added solution for the add tow numbers problem

---
+++ src/main/java/_2_add_two_numbers/Solution.java
@@ -0,0 +1,67 @@
+package _2_add_two_numbers;
+
+import java.util.Objects;
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode root = new ListNode(0);
+        ListNode c = root;
+        int carry = 0;
+        while(l1 != null || l2 != null) {
+            if (l1 != null) {
+                carry += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                carry += l2.val;
+                l2 = l2.next;
+            }
+            c.val = carry%10;
+            carry = carry/10;
+
+
+
+            if (l1 != null || l2 != null) {
+                c.next = new ListNode(0);
+                c = c.next;
+            }
+        }
+        if (carry != 0) {
+            c.next = new ListNode(carry);
+        }
+        return root;
+    }
+}
+
+class ListNode{
+    ListNode next;
+    int val;
+
+    public ListNode(int carry) {
+        this.val = carry;
+    }
+
+    public ListNode addNext(int n){
+        if (next == null) {
+            next = new ListNode(n);
+        } else {
+            next.addNext(n);
+        }
+        return next;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ListNode listNode = (ListNode) o;
+        return val == listNode.val &&
+                Objects.equals(next, listNode.next);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(next, val);
+    }
+}
---
+++ src/test/java/_2_add_two_numbers/SolutionTest.java
@@ -0,0 +1,25 @@
+package _2_add_two_numbers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    public void addTwoNumbersTest(){
+
+        ListNode l1 = new ListNode(2); // [2,4,3]
+        l1.addNext(4).addNext(3);
+        ListNode l2 = new ListNode(5);// [5,6,4]
+        l2.addNext(6).addNext(4);
+
+        Solution s = new Solution();
+        ListNode actual = s.addTwoNumbers(l1, l2);
+
+        var expected = new ListNode(7); // [7, 0, 8]
+        expected.addNext(0).addNext(8);
+
+        assertEquals(expected, actual);
+    }
+}
