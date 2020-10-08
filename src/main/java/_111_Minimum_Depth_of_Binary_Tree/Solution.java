be649b359283df938c4bd7989dfdad2740c7cc99
Nabil <n.elhaououari@gmail.com>
2020-10-08 22:19:55 +0100

    Added a solution for the min depth problem

---
+++ src/main/java/_111_Minimum_Depth_of_Binary_Tree/Solution.java
@@ -0,0 +1,19 @@
+package _111_Minimum_Depth_of_Binary_Tree;
+
+
+import utils.TreeNode;
+
+class Solution {
+    public int minDepth(TreeNode root) {
+        if(root==null)
+            return 0;
+        if(root.left==null && root.right==null)
+            return 1;
+        if(root.right==null)
+            return minDepth(root.left)+1;
+        if(root.left==null)
+            return minDepth(root.right)+1;
+        return Math.min(minDepth(root.left),minDepth(root.right))+1;
+
+    }
+}
