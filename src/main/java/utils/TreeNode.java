b882ee601428d0b41ae8cc9c2fde737b20056b38
elhaouari <n.elhaououari@gmail.com>
2020-10-26 21:21:08 +0100

    added the equals method the TreeNode classk

--- src/main/java/utils/TreeNode.java
+++ src/main/java/utils/TreeNode.java
@@ -1,5 +1,7 @@
 package utils;
 
+import java.util.Objects;
+
 public class TreeNode{
     public int val;
     public TreeNode left;
@@ -17,4 +19,18 @@ public class TreeNode{
         this.right = new TreeNode(right);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TreeNode treeNode = (TreeNode) o;
+        return val == treeNode.val &&
+                Objects.equals(left, treeNode.left) &&
+                Objects.equals(right, treeNode.right);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(val, left, right);
+    }
 }
