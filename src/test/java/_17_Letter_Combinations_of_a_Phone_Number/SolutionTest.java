a695ed53602a8c709264827f861a4a27a943bf6a
elhaouari <n.elhaououari@gmail.com>
2020-11-01 00:04:16 +0100

    fixed the test unit

--- src/test/java/_17_Letter_Combinations_of_a_Phone_Number/SolutionTest.java
+++ src/test/java/_17_Letter_Combinations_of_a_Phone_Number/SolutionTest.java
@@ -1,5 +1,24 @@
+package _17_Letter_Combinations_of_a_Phone_Number;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SolutionTest {
 
+    @Test
+    public void test(){
+        var expected = new ArrayList<>(List.of("ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"));
+
+        Solution s = new Solution();
+        var actual = s.letterCombinations("23");
+        Collections.sort(actual);
+        Collections.sort(expected);
+
+        assertTrue(expected.equals(actual));
+    }
 }
