4b7997574eb09cb512f29864ce2b5c980923f701
elhaouari <n.elhaououari@gmail.com>
2020-11-14 23:18:14 +0100

    added a solution for the single number 2

---
+++ src/main/java/_137_SingleNumber_II/Solution.java
@@ -0,0 +1,22 @@
+package _137_SingleNumber_II;
+
+import java.util.Arrays;
+
+/**
+ * Problem link: https://leetcode.com/problems/single-number-ii/
+ */
+public class Solution {
+
+    public int singleNumber(int[] nums) {
+        int[] bits = new int[32];
+        for (int num : nums) {
+            for (int i = 0; i < bits.length; ++i) {
+                if ((num & (1L << (bits.length - 1 - i))) != 0) {
+                    bits[i] = (bits[i] + 1) % 3;
+                }
+            }
+        }
+
+        return Arrays.stream(bits).reduce(0, (res, elem) -> res * 2 + elem);
+    }
+}
--- src/test/java/_139_WordBreak/SolutionTest.java
+++ src/test/java/_139_WordBreak/SolutionTest.java
@@ -1,5 +1,22 @@
+package _139_WordBreak;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SolutionTest {
 
+    @Test
+    public void testWordBreakE1(){
+        Solution s = new Solution();
+        assertTrue(s.wordBreak("leetcode", List.of("leet", "code")));
+    }
+
+    @Test
+    public void testWordBreakE2(){
+        Solution s = new Solution();
+        assertTrue(s.wordBreak("aaaaaaa", List.of("aaaa","aaa")));
+    }
 }
