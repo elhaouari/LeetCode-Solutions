97b96307a7b79a176503b308e30622a82766dab7
elhaouari <n.elhaououari@gmail.com>
2020-12-13 22:15:34 +0100

    Maximum Number of Coins You Can Get

---
+++ src/main/java/_1561_MaximumNumberOfCoinsYouCanGet/Solution.java
@@ -0,0 +1,18 @@
+package _1561_MaximumNumberOfCoinsYouCanGet;
+
+/**
+ * https://leetcode.com/problems/maximum-number-of-coins-you-can-get/
+ */
+public class Solution {
+
+    public int maxCoins(int[] piles) {
+        piles = Arrays.stream(piles).boxed().sorted().mapToInt(x -> x).toArray();
+
+        int result = 0;
+        for (int i = piles.length / 3; i < piles.length; i += 2) {
+            result += piles[i];
+        }
+
+        return result;
+    }
+}
