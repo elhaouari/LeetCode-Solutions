aee45685341eadc0537bf85db2f57c5d880bf433
elhaouari <n.elhaououari@gmail.com>
2020-12-16 21:10:19 +0100

    Validate Binary Search Tree

---
+++ src/main/java/challenge570/week3/ValidateBinarySearchTree/Solution.java
@@ -0,0 +1,24 @@
+package challenge570.week3.ValidateBinarySearchTree;
+
+import utils.TreeNode;
+
+/**
+ * https://leetcode.com/explore/challenge/card/december-leetcoding-challenge/571/week-3-december-15th-december-21st/3568/
+ */
+public class Solution {
+
+    public boolean isValidBST(TreeNode root) {
+        return check(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    private boolean check(TreeNode root, long lower, long upper) {
+        if (root == null) {
+            return true;
+        }
+        if (root.val < lower || root.val > upper) {
+            return false;
+        }
+
+        return check(root.left, lower, root.val - 1L) && check(root.right, root.val + 1L, upper);
+    }
+}
