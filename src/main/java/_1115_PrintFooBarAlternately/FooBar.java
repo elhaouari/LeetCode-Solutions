671e63642047df1316a9d1c1d309a82c30be0bc9
Nabil <n.elhaououari@gmail.com>
2021-01-12 22:27:34 +0100

    Print FooBar Alternately

---
+++ src/main/java/_1115_PrintFooBarAlternately/FooBar.java
@@ -0,0 +1,38 @@
+package _1115_PrintFooBarAlternately;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ * https://leetcode.com/problems/print-foobar-alternately/
+ */
+public class FooBar {
+
+    private int n;
+    private Semaphore sem1, sem2;
+
+    public FooBar(int n) {
+        this.n = n;
+        this.sem1 = new Semaphore(1);
+        this.sem2 = new Semaphore(0);
+    }
+
+    public void foo(Runnable printFoo) throws InterruptedException {
+
+        for (int i = 0; i < n; i++) {
+            sem1.acquire();
+            // printFoo.run() outputs "foo". Do not change or remove this line.
+            printFoo.run();
+            sem2.release();
+        }
+    }
+
+    public void bar(Runnable printBar) throws InterruptedException {
+
+        for (int i = 0; i < n; i++) {
+            sem2.acquire();
+            // printBar.run() outputs "bar". Do not change or remove this line.
+            printBar.run();
+            sem1.release();
+        }
+    }
+}
