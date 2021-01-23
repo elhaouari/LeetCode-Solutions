e3edc4ebde57ad784d1e4492a36f9dd318b8e712
Nabil <n.elhaououari@gmail.com>
2021-01-23 22:39:59 +0100

    Sort the Matrix Diagonally

---
+++ src/main/java/challenges/januaryLeetcodingChallenge_2021/SortTheMatrixDiagonally/Solution.java
@@ -0,0 +1,21 @@
+package challenges.januaryLeetcodingChallenge_2021.SortTheMatrixDiagonally;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class Solution {
+
+    public int[][] diagonalSort(int[][] mat) {
+        Map<Integer, PriorityQueue<Integer>> map = new HashMap<>();
+        for(int i = 0; i < mat.length; i++)
+            for(int j = 0; j < mat[0].length; j++)
+                map.computeIfAbsent(i - j, p -> new PriorityQueue()).add(mat[i][j]);
+
+        for(int i = 0; i < mat.length; i++)
+            for(int j = 0; j < mat[0].length; j++)
+                mat[i][j] = map.get(i - j).poll();
+
+        return mat;
+    }
+}
