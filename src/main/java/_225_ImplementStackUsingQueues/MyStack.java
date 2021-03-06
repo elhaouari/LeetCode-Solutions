d657bf6fee1448e43f6615d5c62aea2e8c8c3d26
Nabil <n.elhaououari@gmail.com>
2021-03-06 20:50:07 +0100

    Implement Stack using Queues

---
+++ src/main/java/_225_ImplementStackUsingQueues/MyStack.java
@@ -0,0 +1,46 @@
+package _225_ImplementStackUsingQueues;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MyStack {
+    Queue<Integer> queue;
+    Queue<Integer> buffer;
+    int topElement;
+
+    MyStack() {
+        queue = new LinkedList<>();
+        buffer = new LinkedList<Integer>();
+    }
+
+    // Push element x onto stack.
+    public void push(int x) {
+        queue.offer(x);
+        topElement = x;
+    }
+
+    // Removes the element on top of the stack.
+    public void pop() {
+        while (queue.size() > 1) {
+            buffer.offer(queue.poll());
+        }
+        queue.poll();
+
+        while (!buffer.isEmpty()) {
+            int element = buffer.poll();
+            queue.offer(element);
+            topElement = element;
+        }
+    }
+
+    // Get the top element.
+    public int top() {
+        return topElement;
+    }
+
+    // Return whether the stack is empty.
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
+
