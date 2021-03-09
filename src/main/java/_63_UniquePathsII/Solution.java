0999f0408ab69fb8babff890d3909def1dfb4546
Nabil <n.elhaououari@gmail.com>
2021-03-09 22:28:29 +0100

    Unique Paths II

---
+++ src/main/java/_63_UniquePathsII/Solution.java
@@ -0,0 +1,23 @@
+package _63_UniquePathsII;
+
+/**
+ * https://leetcode.com/problems/unique-paths-ii/
+ */
+public class Solution {
+
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int row = obstacleGrid.length;
+        int col = obstacleGrid[0].length;
+        int[][] pathNums = new int[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (obstacleGrid[i][j] == 0) {
+                    pathNums[i][j] = (i == 0 && j == 0) ? 1 : ((i == 0 ? 0
+                            : pathNums[i - 1][j]) + (j == 0 ? 0
+                            : pathNums[i][j - 1]));
+                }
+            }
+        }
+        return pathNums[row - 1][col - 1];
+    }
+}
