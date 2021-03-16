f4cf2719485f747d6dd0e7e9910630336f1a9797
Nabil <n.elhaououari@gmail.com>
2021-03-16 22:20:29 +0100

    Decode Ways

---
+++ src/main/java/_91_DecodeWays/Solution.java
@@ -0,0 +1,30 @@
+package _91_DecodeWays;
+
+class Solution {
+    public int numDecodings(String s) {
+        if (s.isEmpty()) {
+            return 0;
+        }
+
+        int prev = 0;
+        int curr = 1;
+        for (int i = 0; i < s.length(); ++i) {
+            int next = 0;
+            int oneDigit = Integer.parseInt(s.substring(i, i + 1));
+            if (oneDigit >= 1 && oneDigit <= 9) {
+                next += curr;
+            }
+            if (i != 0) {
+                int twoDigit = Integer.parseInt(s.substring(i - 1, i + 1));
+                if (twoDigit >= 10 && twoDigit <= 26) {
+                    next += prev;
+                }
+            }
+
+            prev = curr;
+            curr = next;
+        }
+
+        return curr;
+    }
+}
