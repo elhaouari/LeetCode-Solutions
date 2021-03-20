a77a3802d4663b78975c7e8061fef67e353c0af0
Nabil <n.elhaououari@gmail.com>
2021-03-20 22:50:49 +0100

    Valid Anagram

---
+++ src/main/java/_242_ValidAnagram/Solution.java
@@ -0,0 +1,20 @@
+package _242_ValidAnagram;
+
+import java.util.stream.Collectors;
+
+/**
+ * https://leetcode.com/problems/valid-anagram/
+ */
+public class Solution {
+
+    public boolean isAnagram(String s, String t) {
+        return toKey(s).equals(toKey(t));
+    }
+
+    String toKey(String str) {
+        return str.chars()
+                .sorted()
+                .mapToObj(ch -> String.valueOf((char) ch))
+                .collect(Collectors.joining());
+    }
+}
