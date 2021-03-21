a7828924ae0d7d572395dd6c9f4b6c7228e9902b
Nabil <n.elhaououari@gmail.com>
2021-03-21 22:39:05 +0100

    Minimum ASCII Delete Sum for Two Strings

--- src/main/java/_242_ValidAnagram/Solution.java
+++ src/main/java/_242_ValidAnagram/Solution.java
@@ -7,14 +7,14 @@ import java.util.stream.Collectors;
  */
 public class Solution {
 
-    public boolean isAnagram(String s, String t) {
-        return toKey(s).equals(toKey(t));
-    }
+        public boolean isAnagram(String s, String t) {
+            return toKey(s).equals(toKey(t));
+        }
 
-    String toKey(String str) {
-        return str.chars()
-                .sorted()
-                .mapToObj(ch -> String.valueOf((char) ch))
-                .collect(Collectors.joining());
-    }
+        String toKey(String str) {
+            return str.chars()
+                    .sorted()
+                    .mapToObj(ch -> String.valueOf((char) ch))
+                    .collect(Collectors.joining());
+        }
 }
