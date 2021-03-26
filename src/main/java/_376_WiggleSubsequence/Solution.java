27e11a23042ee328fa8ead66097e80a8dee7859c
Nabil <n.elhaououari@gmail.com>
2021-03-26 22:53:40 +0100

    Wiggle Subsequence

---
+++ src/main/java/_376_WiggleSubsequence/Solution.java
@@ -0,0 +1,23 @@
+package _376_WiggleSubsequence;
+
+/**
+ * https://leetcode.com/problems/wiggle-subsequence/
+ */
+public class Solution {
+
+    public int wiggleMaxLength(int[] nums) {
+        return Math.max(search(nums, true), search(nums, false));
+    }
+
+    int search(int[] nums, boolean upOrDown) {
+        int maxLength = 1;
+        for (int i = 1; i < nums.length; ++i) {
+            if ((upOrDown && nums[i] > nums[i - 1]) || (!upOrDown && nums[i] < nums[i - 1])) {
+                ++maxLength;
+                upOrDown = !upOrDown;
+            }
+        }
+
+        return maxLength;
+    }
+}
