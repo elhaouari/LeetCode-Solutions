79b7df90dc6bb4f5374909dac927b5a5816e66cb
Nabil <n.elhaououari@gmail.com>
2021-06-02 23:47:34 +0100

    Nim Game

---
+++ src/main/java/_292_NimGame/Solution.java
@@ -0,0 +1,15 @@
+package _292_NimGame;
+
+/**
+ * https://leetcode.com/problems/nim-game/
+ */
+public class Solution {
+
+    public boolean canWinNim(int n) {
+        return (n % 4 != 0);
+    }
+}
+
+
+
+
