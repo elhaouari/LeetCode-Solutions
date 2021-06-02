a2e047781f0eecd14055bcc13788fb799de89085
Nabil <n.elhaououari@gmail.com>
2021-06-02 23:50:35 +0100

    Divisor Game

---
+++ src/main/java/_1025_DivisorGame/Solution.java
@@ -0,0 +1,14 @@
+package _1025_DivisorGame;
+
+/**
+ * https://leetcode.com/problems/divisor-game/
+ */
+public class Solution {
+
+    public boolean divisorGame(int n) {
+        if(n%2==0){
+            return true;
+        }
+        return false;
+    }
+}
